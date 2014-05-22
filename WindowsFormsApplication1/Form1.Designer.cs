0a6952a1f2f2f9437a8ec3b9839a8d2732151e76
Calle Persson <callepersson94@telia.com>
2014-05-22 17:42:30 +0200

    Added files

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,193 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button6 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(79, 45);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(50, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "^";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button1_MouseDown);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(127, 101);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(50, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = ">";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button2_MouseDown);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(29, 101);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(50, 50);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "<";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button3_MouseDown);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(79, 157);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(50, 50);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "\\/";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.MouseDown += new System.Windows.Forms.MouseEventHandler(this.button4_MouseDown);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(231, 328);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(75, 23);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Connect";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 328);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 20);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.Text = "ws://127.0.0.1:50007";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 331);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(45, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Address";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(228, 30);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Controller not connected";
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(312, 328);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "Disconnect";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(57, 312);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "Not Connected";
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(231, 56);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(177, 251);
+            this.listBox1.TabIndex = 10;
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(437, 360);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "BWC Client";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Threading;
+using WebSocketSharp;
+using WebSocketSharp.Server;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+
+        private bool connected = false;
+
+        private List<string> list = new List<string>();
+        private WebSocket client;
+        public Form1()
+        {
+
+
+            InitializeComponent();
+            listBox1.DataSource = list;
+            timer1.Start();
+            
+
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!connected)
+            {
+                client = new WebSocket(textBox1.Text);
+
+                client.OnOpen += (sender2, e2) =>
+                 connected = true;
+
+                client.OnClose += (sender2, e2) =>
+                 connected = false;
+
+                client.Connect();
+            }
+
+
+
+            
+            
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (connected) client.Close();
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            GamePadState gamePadState = GamePad.GetState(PlayerIndex.One);
+            
+            if (gamePadState.IsConnected)
+            {
+                label2.Text = "Controller is connected";
+               
+                if (connected)
+                {
+                   
+                    if (gamePadState.Buttons.A == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        Forward();
+                        
+                    }
+                    else if (gamePadState.Buttons.B == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        Back();
+                    }
+                    else if (gamePadState.Buttons.LeftShoulder == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        Left();
+
+                    }
+                    else if (gamePadState.Buttons.RightShoulder == Microsoft.Xna.Framework.Input.ButtonState.Pressed)
+                    {
+                        Right();
+                    }
+                }
+                
+            }
+            else label2.Text = "Controller is not connected";
+
+            if (connected) label3.Text = "Connected";
+            else label3.Text = "Not Connected";
+            Thread.Sleep(100);
+            
+        }
+
+        void Forward()
+        {
+            if (connected)
+            {
+                client.Send("forward");
+                list.Add("forward");
+                Refresh();
+            }
+        }
+
+        void Left()
+        {
+            if (connected)
+            {
+                client.Send("left");
+                list.Add("left");
+                Refresh();
+            }
+        }
+
+        void Right()
+        {
+            if (connected)
+            {
+                client.Send("right");
+                list.Add("right");
+                Refresh();
+            }
+        }
+
+        void Back()
+        {
+            if (connected)
+            {
+                client.Send("back");
+                list.Add("back");
+                Refresh();
+            }
+        }
+
+
+        void Refresh()
+        {
+            listBox1.DataSource = null;
+            listBox1.DataSource = list;
+        }
+
+
+
+
+
+        private void button1_MouseDown(object sender, MouseEventArgs e)
+        {
+            Forward();
+            
+        }
+
+        private void button3_MouseDown(object sender, MouseEventArgs e)
+        {
+            Left();
+
+        }
+
+        private void button2_MouseDown(object sender, MouseEventArgs e)
+        {
+            Right();
+        }
+
+        private void button4_MouseDown(object sender, MouseEventArgs e)
+        {
+            Back();
+        }
+
+
+    }
+}
